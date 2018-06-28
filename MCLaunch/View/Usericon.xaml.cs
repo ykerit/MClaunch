ba6c1a1487bf9849e599ca07e07025761c440c19
凯 袁 <604079879@qq.com>
2018-06-28 10:35:53 +0800

    0.1.3

---
+++ MCLaunch/View/Usericon.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MCLaunch.View
+{
+    /// <summary>
+    /// usericon.xaml 的交互逻辑
+    /// </summary>
+    public partial class usericon : UserControl
+    {
+        private object evriment;
+
+        public usericon()
+        {
+            InitializeComponent();
+            /*using (BinaryReader loader = new BinaryReader(File.Open(@".\image\icon1.gif", FileMode.Open)))
+            {
+                FileInfo fd = new FileInfo(@"/image/icon1.gif");
+                int Length = (int)fd.Length;
+                byte[] buf = new byte[Length];
+                buf = loader.ReadBytes((int)fd.Length);
+                loader.Dispose();
+                loader.Close();
+                //开始加载图像
+                BitmapImage bim = new BitmapImage();
+                bim.BeginInit();
+                bim.StreamSource = new MemoryStream(buf);
+                bim.EndInit();
+                icon.Source = bim;
+                GC.Collect(); //强制回收资源
+            }*/
+
+        }
+        private void openimage()
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "JAVA主程序|*.exe";
+            fdlg.RestoreDirectory = false;
+            if ((bool)fdlg.ShowDialog())
+            {
+                 System.IO.Path.GetFullPath(fdlg.FileName);
+            }
+        }
+
+    }
+}
